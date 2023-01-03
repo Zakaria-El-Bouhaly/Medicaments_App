36cf064a72f42645ac69cfeb2713478d2bdf573c
Your Name <you@example.com>
2023-01-03 21:39:31 +0100

    fixed fragment switching

--- app/src/main/java/com/example1/projectapp/MainActivity.kt
+++ app/src/main/java/com/example1/projectapp/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example1.projectapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example1.projectapp.databinding.ActivityMainBinding
 import com.example1.projectapp.helpers.BottomNavigationBehavior
+import androidx.navigation.ui.setupWithNavController
+
 
 class MainActivity : AppCompatActivity() {
-    private  lateinit var  binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val layoutParams = binding.bottomNavigationView.layoutParams as CoordinatorLayout.LayoutParams
+        val layoutParams =
+            binding.bottomNavigationView.layoutParams as CoordinatorLayout.LayoutParams
         layoutParams.behavior = BottomNavigationBehavior()
+
+        val btm_navigation = binding.bottomNavigationView
+        val navController = findNavController(
+            R.id.nav_host_fragment
+        )
+        btm_navigation.setupWithNavController(navController)
+
+
     }
 }
--- app/src/main/java/com/example1/projectapp/MyListFragment.kt
+++ app/src/main/java/com/example1/projectapp/MyListFragment.kt
@@ -6,26 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.example1.projectapp.databinding.FragmentWichListBinding
+import com.example1.projectapp.databinding.FragmentMylistBinding
+import com.example1.projectapp.databinding.FragmentSearchBinding
 
+class MyListFragment : Fragment() {
 
-class WichListFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val binding : FragmentWichListBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_wich_list, container, false);
+    ): View? {
+        val binding: FragmentMylistBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_mylist, container, false);
         return binding.root
     }
 
     companion object {
 
         @JvmStatic
-        fun newInstance() = WichListFragment()
+        fun newInstance() = MyListFragment()
     }
 }

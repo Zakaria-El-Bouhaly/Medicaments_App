d7bbe79152214fd54f0afdfa33443605ee0d2fd4
yusuf0x <youssefmanssouri87@gmail.com>
2023-01-03 19:19:54 -0500

    update_v1

---
+++ app/src/main/java/com/example1/projectapp/ItemDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.example1.projectapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_details)
+    }
+}
